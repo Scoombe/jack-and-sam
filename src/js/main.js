2f402cac7e228283bbfa2a024cb25bad49d5e86e
scoombe <skatesam95@gmail.com>
2016-08-10 17:28:28 +0100

    Adding timeouts.
    TODO:
    Add the javascript intervals on the dots so they time out,
    Add the a click handler on the dots so they remove and the user gets a point,
    Display the score,
    Randomizing the time it takes create a new dot using set timeout.

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,12 @@ $(function(){
     var pageWidth;
     //object holding all of the timers
     var timers={};
+    //holds the count of dots
+    var dotCount = 0;
     //bool for when the start button is pressed.
     var btnPress;
+    //score
+    var score
     console.log("main.js loaded");
     resize();
 
@@ -23,10 +27,9 @@ $(function(){
         if(!$('#play-button').hasClass("hidden")){
             $("#play-button").css("position","absolute");
             $("#play-button").css({'left': pageWidth/2+'px','top': pageHeight/2+'px'});
-            console.log("The page height is "+ pageHeight + " the page length is "+ pageWidth);
         }
     };
-    //when the play button is pressed hdie it
+    //when the play button is pressed hide it
     $("#play-button").on("click",function(){
         $("#play-button").addClass("hidden");
         btnPress = true;
@@ -44,18 +47,40 @@ $(function(){
 
     //funciton for adding a dot to the page
     function createDot(){
+        dotCount += 1
         var pos = randomPosition()
-        //creating a div and assigning a click event and passing the button to the dotClick function
-        $("body").append("<div class='dot' style='position:absolute;left:"+pos["left"]+"px;top:"+pos["top"]+"px'>")
-        $("</div>",
-            {
-                "class": "dot",
-                click: function () {
-                    dotClick($(this));
-                },
-                //adding the random pos to the left and top attrs
-                left: pos["left"],
-                top: pos["top"]
-            });
+        //adding a timeout on the page for the dot so that after a certain amount of
+        // mileseconds the user loses if the timeout carrys out
+        timers["dot"+dotCount]= setTimeout(function(){
+            dotTimeout();
+        },3000)
+        //string for the div
+        var divStr = "<div class='dot'  data-dot='"+dotCount+"' style='position:absolute;left:"+
+            pos["left"]+"px;top:"+pos["top"]+"px'>"
+        //adding the dot div to the page
+        $("body").append(divStr)
+    }
+    $("body").on("click",".dot",function(){
+        //the dot number is a attr added to the html element
+        var dotNo = $(this).attr("data-dot")
+        //clearing the timeout on the dot
+        clearTimeout(timers["dot"+dotNo]);
+        //removing the dot from the dom
+        $(this).remove();
+        // creating a  new dot.
+        createDot();
+    });
+
+    function dotTimeout(){
+        //removing the hidden class and calling the resize method
+        $("#play-button").removeClass("hidden");
+        //removing all of the dots
+        $(".dot").remove();
+        //calling the resize so the play button is still centered
+        resize();
+        //creating the lose text
+        var loseTxt = $("<p/>",{text:"You have lost the game your score was "});
+        //prefixing it to the div
+        $("#body-fluid").prefix(loseTxt);
     }
 });
